c5b81e542a88d877b95d1774a43a227e106ee209
Roy Meshulam <roy.meshulam@gmail.com>
2025-05-16 16:26:40 +0200

    update reqs

--- finbert_utils.py
+++ finbert_utils.py
@@ -1,11 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-from typing import Tuple 
+from typing import Tuple
+
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(device)
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(
+    device
+)
 labels = ["positive", "negative", "neutral"]
+
 
 def estimate_sentiment(news):
     if news:
@@ -23,5 +27,7 @@ def estimate_sentiment(news):
 
 
 if __name__ == "__main__":
-    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!','traders were displeased!'])
+    tensor, sentiment = estimate_sentiment(
+        ["markets responded positive to the news!", "traders were happy!"]
+    )
     print(tensor, sentiment)
